9e51af93948c561e7049972f92eb73a78466c745
Jimin Park <parkjimin0@gmail.com>
2019-09-29 16:58:40 -0400

    Scaffold inventory

---
+++ db/migrate/20190929205817_create_inventories.rb
@@ -0,0 +1,11 @@
+class CreateInventories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :inventories do |t|
+      t.string :name
+      t.boolean :damaged
+      t.references :warehouse, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
