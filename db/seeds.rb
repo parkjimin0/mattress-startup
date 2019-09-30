36beea1462d99d7ed2e77bc3b0ef9c10298bcc04
Jimin Park <parkjimin0@gmail.com>
2019-09-29 22:43:41 -0400

    Create dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Warehouse.destroy_all
+Inventory.destroy_all
+
+ActiveRecord::Base.connection.reset_pk_sequence!("warehouses")
+ActiveRecord::Base.connection.reset_pk_sequence!("inventories")
+
+warehouse_list = [
+    "Jimin's Warehouse",
+    "Tim's Warehouse",
+    "David's Warehouse", 
+    "Julio's Warehouse",
+    "Anthony's Warehouse", 
+]
+
+warehouse_list.each do |name|
+    Warehouse.create( name: name, location: "USA" )
+end
+
+
+Warehouse.all.each do |warehouse| 
+    warehouse.inventories.create(:name => "Pretty Mattress" )
+    warehouse.inventories.create(:name => "Ugly Mattress", damaged: true )
+    warehouse.inventories.create(:name => "Nice Mattress" )
+    warehouse.inventories.create(:name => "Smelly Mattress", damaged: true )
+    warehouse.inventories.create(:name => "New Mattress" )
+    warehouse.inventories.create(:name => "Old Mattress", damaged: true )
+end
