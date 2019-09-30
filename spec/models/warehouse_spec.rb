89ba1f2716e1cbae2ba1e15836dd85e5d8dd7446
Jimin Park <parkjimin0@gmail.com>
2019-09-29 22:22:09 -0400

    create warehouse model specs

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Warehouse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Warehouse do
+	context 'associations' do
+		it "ensure Warehouse model has a one to many relationship with Inventory model" do
+			should have_many(:inventories).dependent(:destroy) 
+		end
+	end
+
+	context 'validations' do
+		it "is valid if name is present" do
+			should validate_presence_of(:name) 
+    end
+    
+    it "is valid if location is present" do
+			should validate_presence_of(:location) 
+		end
+	end
 end
